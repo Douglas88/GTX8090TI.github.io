419734ece0b968a713b319acb7de72e4a33ba886
GTX8090TI <328767586@qq.com>
2021-02-22 23:44:45 +0800

    Site updated: 2021-02-22 23:44:43

--- js/yejiao.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(e) {
-    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Lete乐特');
-});
