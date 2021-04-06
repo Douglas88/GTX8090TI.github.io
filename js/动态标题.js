b16a47c4633da128b4b2cd90e682f3cf4da5c6d6
GTX8090TI <328767586@qq.com>
2021-04-06 13:52:27 +0800

    Site updated: 2021-04-06 13:52:25

--- js/动态标题.js
+++ js/动态标题.js
@@ -1,1 +1,16 @@
-"use strict";var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",function(){document.hidden?($('[rel="icon"]').attr("href","/img/favicon.ico"),document.title="溜了嘛",clearTimeout(titleTime)):($('[rel="icon"]').attr("href","/img/favicon.ico"),document.title="回来了嘛"+OriginTitle,titleTime=setTimeout(function(){document.title=OriginTitle},2e3))});
+// 可爱的Title
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function() {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/img/favicon.ico");
+        document.title = '溜了嘛';
+        clearTimeout(titleTime);
+    } else {
+        $('[rel="icon"]').attr('href', "/img/favicon.ico");
+        document.title = '回来了嘛' + OriginTitle;
+        titleTime = setTimeout(function() {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
