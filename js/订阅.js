34743598c949680105c7b0fbb787ae495b89dd8d
GTX8090TI <328767586@qq.com>
2021-04-12 18:18:19 +0800

    Site updated: 2021-04-12 18:18:17

--- js/订阅.js
+++ js/订阅.js
@@ -1,5 +1,1 @@
-<!-- start webpushr tracking code --> 
-<script>(function(w,d, s, id) {if(typeof(w.webpushr)!=='undefined') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.async=1;js.src = "https://cdn.webpushr.com/app.min.js";
-fjs.parentNode.appendChild(js);}(window,document, 'script', 'webpushr-jssdk'));
-webpushr('setup',{'key':'BGmdr_6hjZeQwNLDw1mMmw8wP4t_oRBBi6wUsmOpYhxoJ_MIR0gC56rGktEIeDzARqSsNCTbctqD3UMR7n9GCZo', 'integration':'popup'  });</script>
-<!-- end webpushr tracking code -->
+<!-- start webpushr code --> <script>(function(w,d, s, id) {if(typeof(w.webpushr)!=='undefined') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.async=1;js.src = "https://cdn.webpushr.com/app.min.js";fjs.parentNode.appendChild(js);}(window,document, 'script', 'webpushr-jssdk'));webpushr('setup',{'key':'BAHAcVG8oQaosAMhISJv9bv0GWkFG96tBfegFm_x8AWXEJyqcamarpGJ0EbmRrtjq5RfrPwiZi8Ld5mZwZ_NnlQ' });</script><!-- end webpushr code -->
